9187fc37f20cda9975444cc6c67ded8c2454670d
Pandeypushpesh <pushpeshkumary2@gmail.com>
2025-11-02 01:29:42 +0530

    format dotenv configuration for improved readability

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import mongoose from "mongoose";
 // import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: "./env" });
+dotenv.config({
+     path: "./env"
+     });
+
+     
 import { app } from "./app.js";
 
 
